d51666b62fef22b3023bcc8881119de25275ee9f
yahya <nimsize@gmail.com>
2023-02-27 17:26:03 +0400

    creation

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,119 +9,140 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _buttonPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-          child: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Icon(
-            Icons.phone_android,
-            size: 100,
-          ),
-
-          SizedBox(height: 10),
-          //Hello again!
-          Text(
-            'Hello!',
-            style: GoogleFonts.lato(
-              fontSize: 36,
-            ),
-          ),
-
-          SizedBox(height: 10),
-          Text(
-            'Welcome back ,you\'ve been missed !',
-            style: TextStyle(
-              fontSize: 24,
-            ),
-          ),
-          SizedBox(height: 20),
-          //email textfield
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Email',
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-//password
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "password",
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          //sign in button
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Center(
-                  child: Text(
-                "Sign In",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              )),
-            ),
-          ),
-          SizedBox(height: 25),
-
-//not a member ? Register now
-
-          Row(
+        child: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Icon(
+                Icons.phone_android,
+                size: 100,
+              ),
+              SizedBox(height: 10),
               Text(
-                'Not a Member?',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+                'Hello!',
+                style: GoogleFonts.lato(
+                  fontSize: 36,
                 ),
               ),
+              SizedBox(height: 10),
               Text(
-                ' Register now',
+                'Welcome back, you\'ve been missed!',
                 style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
                 ),
-              )
+              ),
+              SizedBox(height: 20),
+              // email textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              // password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "password",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              // sign in button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  onTapDown: (_) {
+                    setState(() {
+                      _buttonPressed = true;
+                    });
+                  },
+                  onTapUp: (_) {
+                    setState(() {
+                      _buttonPressed = false;
+                    });
+                  },
+                  onTapCancel: () {
+                    setState(() {
+                      _buttonPressed = false;
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: _buttonPressed
+                          ? Colors.deepPurple[700]
+                          : Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+              // not a member ? Register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a Member?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    ' Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
-        ]),
-      )),
+        ),
+      ),
     );
   }
 }
